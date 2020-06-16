a028c0b081dcecf884bfa1e4ea5ec9b1fc811855
anudeike <ikechukwuanude@gmail.com>
2020-06-16 16:24:15 -0500

    refractored code and put it in process

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const dotenv = require('dotenv').config()
 const CoreNLP = require('corenlp')
 const fs = require('fs');
 const util = require('util');
+let Process = require('./process');
 
 // assumes that the nlp is running on port 9000
 app.use(morgan('tiny'));
@@ -24,46 +25,9 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-// get the properties and pipeline
-const props = new CoreNLP.Properties({
-    annotators: 'tokenize,ssplit,pos,lemma,ner,parse',
-}); 
+const pro = new Process('tokenize,ssplit,pos,lemma,ner,parse', "English");
+pro.run('I love your portraits! how much do you charge for logos?');
 
-// create a new pipline, should only be created once per request
-const pipeline = new CoreNLP.Pipeline(props, 'English');
-
-// must use the default
-const expr = new CoreNLP.default.simple.Expression(
-    'I love your portraits! how much do you charge for logos?',
-    '[]* (how much) []* (charge) []* (logos|portraits|book illustrations)');
-
-pipeline.annotateTokensRegex(expr, true)
-    .then(expr => {
-        // returns an Expression Object
-        // should cycle through each one of the sentences and see if there is a match
-        expr.sentence(1).matches().map(match => {
-            // then you cycle through this
-            console.log('matched: ', match._data[1]); // can access the inner matches
-        });
-    }).catch(err => {
-        console.log('err', err)
-    })
-
-//console.log(sent)
-// pipeline.annotate(sent)
-//     .then(sent => {
-//         // parsed tree
-//         var parsed = sent.parse();
-
-//         // json tree - write to file
-//         var jsonTree = CoreNLP.default.util.Tree.fromSentence(sent).dump()
-//         var path = "data.json";
-
-//         console.log(jsonTree)
-//     })
-//     .catch(err => {
-//         console.log('err', err);
-//     });
 // // listeners
 // const port = process.env.PORT || 4000
 // app.listen(port, () => {
--- process.js
+++ process.js
@@ -1,4 +1,57 @@
+// imports 
+const CoreNLP = require('corenlp')
+
 // export it one time
 module.exports = class Process {
+
+    
+    constructor(annotators, language){
+        // set the pipeline
+        // might want to wrap this into a try catch statement
+        this.pipeline = this.startPipeline(annotators, language)
+        
+        // holds all the extra variable that this needs
+        this.state = {
+            patterns: ['[]* (how much) []* (charge) []* (logos|portraits|book illustrations)'],
+            products: ['logos', 'portraits', 'book illustrations'],
+            context: {
+                history: []
+            }
+        }
+
+    }
+
+    // initialize the pipeline
+    startPipeline(annotators, lang){
+        
+        const props = new CoreNLP.Properties({
+            annotators: annotators,
+        }); 
+
+        const pipeline = new CoreNLP.Pipeline(props, lang);
+
+        return pipeline
+    }
+    
+    // process an input - should be called on the on input event
+    run(incomingMsg){
+
+        // create the expression
+        const expr = new CoreNLP.default.simple.Expression(
+            incomingMsg,
+            this.state.patterns);
+
+        this.pipeline.annotateTokensRegex(expr, true)
+            .then(expr => {
+            // returns an Expression Object
+            // should cycle through each one of the sentences and see if there is a match
+            expr.sentence(1).matches().map(match => {
+                // then you cycle through this
+                console.log('matched: ', match._data[1]); // can access the inner matches
+            });
+        }).catch(err => {
+            console.log('err', err)
+        })
+    }
 
 }
