fbc63006ee75f4cff6df0912707a415e929c1784
anudeike <ikechukwuanude@gmail.com>
2020-06-16 21:11:24 -0500

    done refractoring for the most part. am working on collecting information for the state

--- process.js
+++ process.js
@@ -1,6 +1,8 @@
 // imports 
 const CoreNLP = require('corenlp')
 
+// DOCS: https://gerardobort.github.io/node-corenlp/docs/index.html
+
 // export it one time
 module.exports = class Process {
 
@@ -14,8 +16,17 @@ module.exports = class Process {
         this.state = {
             patterns: ['[]* (how much) []* (charge) []* (logos|portraits|book illustrations)'],
             products: ['logos', 'portraits', 'book illustrations'],
+            rules: [
+                {
+                    // ruleName, pattern, which token mode it uses
+                    "name": "Cost Request",
+                    "pattern": "[]* (how much) []* (charge) []* (logos|portraits|book illustrations)",
+                    "mode": "TOKEN"
+                }
+            ],
             context: {
-                history: []
+                history: [],
+                
             }
         }
 
@@ -23,42 +34,88 @@ module.exports = class Process {
 
     // initialize the pipeline
     startPipeline(annotators, lang){
-        
         const props = new CoreNLP.Properties({
             annotators: annotators,
         }); 
-
         const pipeline = new CoreNLP.Pipeline(props, lang);
-
         return pipeline
     }
+
+    // update the state - simple update
+    updateState(key, value){
+        this.state[key] = value;
+    }
+
+    // create the expression
+    initExpression(incoming, pattern){
+        return new CoreNLP.default.simple.Expression(
+            incoming,
+            pattern);
+    }
     
+    // analyze the tokens
+    runPipeline(expr, mode){
+
+        switch(mode){
+            case "TOKEN":
+                // to do
+                this.pipeline.annotateTokensRegex(expr, true)
+                    .then(expr => {
+                    //cycle thru each of the sentences
+                    for(var i = 0; i <= expr.sentence.length; i++){
+                        // if there is a match in the sentence
+                        if(expr.sentence(i).matches().length > 0) {
+                            expr.sentence(i).matches().map(match => {
+                                console.log(match._data[1]);
+                            })
+                        }
+                    }
+                }).catch(err => {
+                    console.log('err', err)
+                })
+                break;
+            default:
+                throw "Only the TOKEN mode is implemented!"
+        }
+    }
+
     // process an input - should be called on the on input event
-    run(incomingMsg){
+    async run(incomingMsg){
 
-        // create the expression
-        const expr = new CoreNLP.default.simple.Expression(
-            incomingMsg,
-            this.state.patterns);
+        // // for each rule
+        // await Promise.all(this.state.rules.map( async (rule) => {
+        //     // create an expression
+        //     const expr = this.initExpression(incomingMsg, rule.pattern);
 
+        //     // run the pipeline
+        //     const result = await this.runPipeline(expr, rule.mode);
+        // }));
+
+        const expr = this.initExpression(incomingMsg, this.state.patterns[0]);
+
+        // this should be in a separate function?
         this.pipeline.annotateTokensRegex(expr, true)
             .then(expr => {
-                // cycle thru each of the sentences
-                for(var i; i < expr.sentence.length; i++){
+                //cycle thru each of the sentences
+                for(var i = 0; i <= expr.sentence.length; i++){
                     // if there is a match in the sentence
-                    if(expr.sentence(i) > 0) {
-                        console.log("matched")
+                    if(expr.sentence(i).matches().length > 0) {
+                        expr.sentence(i).matches().map(match => {
+                            console.log(match._data);
+                        })
                     }
                 }
-            // returns an Expression Object
-            // should cycle through each one of the sentences and see if there is a match
-            expr.sentence(1).matches().map(match => {
-                // then you cycle through this
-                console.log('matched: ', match._data[1]); // can access the inner matches
-            });
+            // // returns an Expression Object
+            // // should cycle through each one of the sentences and see if there is a match
+            // expr.sentence(1).matches().map(match => {
+            //     // then you cycle through this
+            //     console.log('matched: ', match._data[1]); // can access the inner matches
+            // });
         }).catch(err => {
             console.log('err', err)
         })
     }
+
+    
 
 }
